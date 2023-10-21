d60880e0eb7bdb58fd28b58db13c8d274577da9f
Taranko Taras <tarastarnko2@gmail.com>
2023-10-21 16:22:21 +0300

    Aded course bage service and register it

--- neophyte_proj/neophyte_proj/Models/CourseModel/CourseBageDto.cs
+++ neophyte_proj/neophyte_proj/Models/CourseModel/CourseBageDto.cs
@@ -7,6 +7,5 @@
         public string Description { get; set; }
         public int Rating { get; set; }
         public int CourseId { get; set; }
-        public int StudentId { get; set; }
     }
 }
